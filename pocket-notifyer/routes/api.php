d084fc10cfceeb5c00578eb49b32ee01158ff1bc
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-11 15:29:40 -0300

    refact: adjust unecessary import

--- pocket-notifyer/routes/api.php
+++ pocket-notifyer/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Infrastructure\Http\Controllers\EmailController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/email", [EmailController::class, 'send']);
