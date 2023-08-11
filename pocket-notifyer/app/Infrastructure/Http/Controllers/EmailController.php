9e41818a8c9ab02dfcf7b8a9869b0a11bf1bda47
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-11 12:20:47 -0300

    feat: return status created

--- pocket-notifyer/app/Infrastructure/Http/Controllers/EmailController.php
+++ pocket-notifyer/app/Infrastructure/Http/Controllers/EmailController.php
@@ -5,6 +5,7 @@ namespace App\Infrastructure\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Infrastructure\Http\Entity\EmailRequest;
 use App\Service\EmailService;
+use Illuminate\Http\Response;
 
 class EmailController extends Controller
 {
@@ -13,8 +14,10 @@ class EmailController extends Controller
     ) {
     }
 
-    public function send(EmailRequest $emailRequest)
+    public function send(EmailRequest $emailRequest): Response
     {
         $this->emailService->send($emailRequest);
+
+        return response()->noContent(201);
     }
 }
