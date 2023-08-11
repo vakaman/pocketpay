2be1ae458f7159da6c741246aa3fc7429345848c
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-10 22:16:59 -0300

    feat: add email envelope  entity

---
+++ pocket-notifyer/app/Domain/Entity/Email/Envelope.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Domain\Entity\Email;
+
+class Envelope
+{
+    public function __construct(
+        public readonly Header $header,
+        public readonly Body $body
+    ) {
+    }
+
+    public function toArray()
+    {
+        return (array) $this;
+    }
+}
