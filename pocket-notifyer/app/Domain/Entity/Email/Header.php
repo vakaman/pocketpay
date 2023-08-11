1460d305e5d2da81229a36771e778c25546ca0e0
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-11 12:06:08 -0300

    feat: add sender and receiver

--- pocket-notifyer/app/Domain/Entity/Email/Header.php
+++ pocket-notifyer/app/Domain/Entity/Email/Header.php
@@ -10,23 +10,26 @@ class Header
     public readonly string $subject;
     public readonly string $date;
     public readonly string $from;
-    public readonly string $to;
     public readonly string $sender;
+    public readonly string $to;
+    public readonly string $receiver;
     public readonly string $reply_to;
 
     public function __construct(
         string $subject,
         string $date,
         string $from,
-        string $to,
         string $sender,
+        string $to,
+        string $receiver,
         string $reply_to,
     ) {
         $this->subject = Sanitize::string($subject);
         $this->date = Carbon::createFromFormat('Y-m-d H:i:s', Sanitize::string($date));
         $this->from = new Email(Sanitize::email($from));
-        $this->to = new Email(Sanitize::email($to));
         $this->sender = Sanitize::string($sender);
+        $this->to = new Email(Sanitize::email($to));
+        $this->receiver = Sanitize::string($receiver);
         $this->reply_to = Sanitize::string($reply_to);
     }
 
