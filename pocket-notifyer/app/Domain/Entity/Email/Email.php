df8233927dea7af8635f82f13ab8b00db1431974
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-10 22:16:51 -0300

    feat: add email entity

---
+++ pocket-notifyer/app/Domain/Entity/Email/Email.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Domain\Entity\Email;
+
+use App\Util\Sanitize;
+
+class Email
+{
+    private string $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = Sanitize::email($email);
+    }
+
+    public function __toString()
+    {
+        return $this->email;
+    }
+}
