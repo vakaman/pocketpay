73109c2e32604f01802302f41c993a1100b997dc
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-11 21:41:59 -0300

    feat: create database structure

--- pocket-manager/database/migrations/2023_08_11_214103_create_status_table.php
+++ pocket-manager/database/migrations/2023_08_11_214103_create_status_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        if (!Schema::hasTable('status')) {
+            Schema::create('status', function (Blueprint $table) {
+                $table->char('name')->unique()->nullable(false)->primary();
+            });
+        }
     }
 
     /**
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('status');
     }
 };
---
+++ pocket-manager/database/seeders/StatusSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+    }
+}
