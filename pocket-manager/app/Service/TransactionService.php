51fb4dd714418fd99d6085f431b87aa3f238d01a
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:51:04 -0300

    feat: add transaction service

--- pocket-manager/app/Service/TransactionService.php
+++ pocket-manager/app/Service/TransactionService.php
@@ -2,6 +2,29 @@
 
 namespace App\Service;
 
+use App\Domain\Entity\Financial\Transaction;
+use App\Domain\Entity\Financial\TransactionHistory;
+use App\Domain\Repository\TransactionRepositoryInterface;
+
 class TransactionService
 {
+    public function __construct(
+        private WalletService $walletService,
+        private TransactionRepositoryInterface $transactionRepository
+    ) {
+    }
+
+    public function detail(string $id): Transaction
+    {
+        $transaction = $this->transactionRepository->detail($id);
+
+        return $transaction;
+    }
+
+    public function history(string $person, string $wallet): TransactionHistory
+    {
+        $this->walletService->belongsToPerson($wallet, $person);
+
+        return $this->transactionRepository->history($wallet);
+    }
 }
