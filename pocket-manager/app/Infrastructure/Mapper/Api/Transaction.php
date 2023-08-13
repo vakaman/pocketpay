886e1adf28c1fb23f5e6ccc805b9a574d0b2cea6
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:46:20 -0300

    feat: create transaction mapper

---
+++ pocket-manager/app/Infrastructure/Mapper/Api/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Infrastructure\Mapper\Api;
+
+use App\Domain\Entity\Financial\Transaction as FinancialTransaction;
+
+class Transaction
+{
+    public function __construct(
+        private FinancialTransaction $transaction
+    ) {
+    }
+
+    public function response(): array
+    {
+        return [
+            'transaction' => [
+                'id' => $this->transaction->id->value,
+                'from' => $this->transaction->from->value,
+                'to' => $this->transaction->to->value,
+                'value' => $this->transaction->value->toInt(),
+                'created_at' => $this->transaction->created_at,
+                'updated_at' => $this->transaction->updated_at,
+            ]
+        ];
+    }
+}
