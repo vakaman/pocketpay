f58940b0c01c452dfa9028fd2060aecd7b89ea81
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:47:58 -0300

    feat: register required interfaces

--- pocket-manager/app/Providers/AppServiceProvider.php
+++ pocket-manager/app/Providers/AppServiceProvider.php
@@ -2,23 +2,25 @@
 
 namespace App\Providers;
 
+use App\Domain\Repository\PersonRepositoryInterface;
+use App\Domain\Repository\TransactionRepositoryInterface;
+use App\Domain\Repository\WalletRepositoryInterface;
+use App\Infrastructure\Repository\PersonRepository;
+use App\Infrastructure\Repository\TransactionRepository;
+use App\Infrastructure\Repository\WalletRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->app->bind(PersonRepositoryInterface::class, PersonRepository::class);
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
+        $this->app->bind(WalletRepositoryInterface::class, WalletRepository::class);
     }
 }
