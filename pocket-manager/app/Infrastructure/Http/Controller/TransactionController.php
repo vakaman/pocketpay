82bf34be07f33a513e06294f0f4d97ee00efd5e2
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:44:43 -0300

    feat: create transaction controller

--- pocket-manager/app/Infrastructure/Http/Controller/TransactionController.php
+++ pocket-manager/app/Infrastructure/Http/Controller/TransactionController.php
@@ -3,7 +3,10 @@
 namespace App\Infrastructure\Http\Controller;
 
 use App\Http\Controllers\Controller;
+use App\Infrastructure\Mapper\Api\Transaction;
+use App\Infrastructure\Mapper\Api\TransactionHistory;
 use App\Service\TransactionService;
+use Illuminate\Http\JsonResponse;
 
 class TransactionController extends Controller
 {
@@ -12,4 +15,17 @@ class TransactionController extends Controller
     ) {
     }
 
+    public function detail(string $id): JsonResponse
+    {
+        $transaction = new Transaction($this->transactionService->detail($id));
+
+        return response()->json($transaction->response());
+    }
+
+    public function history(string $person, string $wallet): JsonResponse
+    {
+        $transaction = new TransactionHistory($this->transactionService->history($person, $wallet));
+
+        return response()->json($transaction->response());
+    }
 }
