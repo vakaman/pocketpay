2e472fefd703fd762fed872609603ecf4f8cc9c2
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:45:16 -0300

    refact: add resource to ignore json in get method

--- pocket-manager/app/Infrastructure/Http/Entity/ApiRequest.php
+++ pocket-manager/app/Infrastructure/Http/Entity/ApiRequest.php
@@ -9,7 +9,10 @@ abstract class ApiRequest extends FormRequest
 
     public function authorize(): bool
     {
+        if ($this->isMethod('GET')) {
+            return true;
+        }
+
         return $this->isJson();
     }
-
 }
