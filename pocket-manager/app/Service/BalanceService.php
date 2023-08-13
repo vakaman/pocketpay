ad2a3a72c8f1b4f8157366f7899589f6c755b5bc
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:48:31 -0300

    feat: create balance service

--- pocket-manager/app/Service/BalanceService.php
+++ pocket-manager/app/Service/BalanceService.php
@@ -2,6 +2,21 @@
 
 namespace App\Service;
 
+use App\Domain\Entity\Pocket\Wallet;
+use App\Domain\Entity\People\Person;
+use App\Domain\Repository\PersonRepositoryInterface;
+
 class BalanceService
 {
+    public function __construct(
+        private PersonRepositoryInterface $personRepository,
+    ) {
+    }
+
+    public function getBalance(Person $person, Wallet $wallet): int
+    {
+        $personWallet = $this->personRepository->getWallet($person, $wallet);
+
+        return $personWallet->money->toInt();
+    }
 }
