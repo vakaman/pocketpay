df706cde72764088598f331a4acb50cd5aa98983
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:43:25 -0300

    feat: create invalid uuid exception

---
+++ pocket-manager/app/Domain/Exception/InvalidUuidException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Domain\Exception;
+
+class InvalidUuidException extends \Exception
+{
+    public static string $newMessage = 'The value provided for the UUID object is invalid.';
+
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct(self::setMessage($message), $code, $previous);
+    }
+
+    public static function setMessage(string $message): string
+    {
+        return self::$newMessage;
+    }
+}
