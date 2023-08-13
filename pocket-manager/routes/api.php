444bec10072c24be0aa97d72d3ebb81cb39607f4
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:49:56 -0300

    refact: adjust routes

--- pocket-manager/routes/api.php
+++ pocket-manager/routes/api.php
@@ -2,14 +2,15 @@
 
 use App\Infrastructure\Http\Controller\BalanceController;
 use App\Infrastructure\Http\Controller\TransactionController;
+use App\Infrastructure\Http\Controller\WalletController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/balance/{person}/{pocket}", [BalanceController::class, 'balance']);
+Route::get("/balance/{person}/{wallet}", [BalanceController::class, 'balance']);
 
 Route::get("/transaction/{id}", [TransactionController::class, 'detail']);
-Route::get("/transaction/history/{person}/{pocket}", [TransactionController::class, 'history']);
+Route::get("/transaction/history/{person}/{wallet}", [TransactionController::class, 'history']);
 Route::post("/transaction", [TransactionController::class, 'transaction']);
 
-Route::post("/pocket", [PocketController::class, 'create']);
-Route::patch("/pocket", [PocketController::class, 'setMain']);
+Route::post("/wallet", [WalletController::class, 'create']);
+Route::patch("/wallet", [WalletController::class, 'setMain']);
 
