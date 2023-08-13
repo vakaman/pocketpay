00bed9f08c3edeca61e16a893950b52023549f0d
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 00:44:33 -0300

    feat: create balance controller

--- pocket-manager/app/Infrastructure/Http/Controller/BalanceController.php
+++ pocket-manager/app/Infrastructure/Http/Controller/BalanceController.php
@@ -2,10 +2,12 @@
 
 namespace App\Infrastructure\Http\Controller;
 
-use App\Domain\Entity\Person\Person;
-use App\Domain\Entity\Pocket\Pocket;
+use App\Domain\Entity\People\Person;
+use App\Domain\Entity\Pocket\Wallet as PocketWallet;
 use App\Http\Controllers\Controller;
+use App\Infrastructure\Mapper\Api\Balance;
 use App\Service\BalanceService;
+use Illuminate\Http\JsonResponse;
 
 class BalanceController extends Controller
 {
@@ -14,9 +16,15 @@ class BalanceController extends Controller
     ) {
     }
 
-    public function balance(Person $person, Pocket $pocket)
+    public function balance(string $person, string $wallet): JsonResponse
     {
-        dd($person, $pocket);
-    }
+        $balance = new Balance(
+            $this->balanceService->getBalance(
+                new Person($person),
+                new PocketWallet($wallet)
+            )
+        );
 
+        return response()->json($balance->response());
+    }
 }
