b8f93583902696dc708579fbf766af6e6468f48d
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 22:54:57 -0300

    feat: create  cannot transfer funds to same wallet exception

---
+++ pocket-manager/app/Domain/Exception/Transaction/CannotTransferFoundsToSameWalletException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Domain\Exception\Transaction;
+
+class CannotTransferFundsToSameWalletException extends \Exception
+{
+    public static string $newMessage = 'You cannot transfer funds to same wallet.';
+
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct(self::setMessage($message), $code, $previous);
+    }
+
+    public static function setMessage(string $message): string
+    {
+        return self::$newMessage;
+    }
+}
