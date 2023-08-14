37906aa479b0d9bbc2651c3cd741de346393885e
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 22:55:24 -0300

    feat: handle with exceptions

--- pocket-manager/app/Exceptions/Handler.php
+++ pocket-manager/app/Exceptions/Handler.php
@@ -2,29 +2,40 @@
 
 namespace App\Exceptions;
 
+use App\Domain\Exception\WalletDontHaveFundsException;
+use App\Domain\Exception\WalletNotExistsException;
+use App\Infrastructure\Http\Entity\StatusCode;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Illuminate\Http\Request;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
+        $this->reportable(function (\Throwable $e) {
             //
+        });
+
+        $this->renderable(function (WalletNotExistsException $e, Request $request) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'wallet' => $e->getWallet()->id->value,
+                'transaction' => $request->all()
+            ], StatusCode::NOT_FOUND->value);
+        });
+
+        $this->renderable(function (WalletDontHaveFundsException $e, Request $request) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'wallet' => $e->getWalletUuid(),
+                'transaction' => $request->all()
+            ], StatusCode::FORBIDDEN->value);
         });
     }
 }
