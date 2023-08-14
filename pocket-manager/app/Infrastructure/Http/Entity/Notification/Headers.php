b63b7325d8a2e9767555280560a6ac2e66e51235
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-13 23:05:23 -0300

    feat: type fields

--- pocket-manager/app/Infrastructure/Http/Entity/Notification/Headers.php
+++ pocket-manager/app/Infrastructure/Http/Entity/Notification/Headers.php
@@ -3,34 +3,35 @@
 namespace App\Infrastructure\Http\Entity\Notification;
 
 use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\Name;
 use App\Util\Sanitize;
 use Illuminate\Support\Carbon;
 
 class Headers
 {
     public readonly string $subject;
-    public readonly string $from;
-    public readonly string $sender;
-    public readonly string $to;
-    public readonly string $receiver;
+    public readonly Email $from;
+    public readonly Name $sender;
+    public readonly Email $to;
+    public readonly Name $receiver;
     public readonly ?string $reply_to;
     public readonly ?string $date;
 
     public function __construct(
         string $subject,
-        string $from,
-        string $sender,
-        string $to,
-        string $receiver,
+        Email $from,
+        Name $sender,
+        Email $to,
+        Name $receiver,
         ?string $reply_to = null,
         ?string $date = null,
     ) {
         $this->subject = Sanitize::string($subject);
         $this->date = Carbon::createFromFormat('Y-m-d H:i:s', Sanitize::string($date));
-        $this->from = new Email($from);
-        $this->sender = Sanitize::string($sender);
-        $this->to = new Email($to);
-        $this->receiver = Sanitize::string($receiver);
+        $this->from = $from;
+        $this->sender = $sender;
+        $this->to = $to;
+        $this->receiver = $receiver;
         $this->reply_to = Sanitize::string($reply_to) ?? $this->from;
     }
 
