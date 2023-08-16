6686e0f038c7fdee5c561879d7232442f5d07821
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-16 08:13:14 -0300

    refact: adjust fields and to array method

--- pocket-manager/app/Infrastructure/Http/Entity/Notification/Headers.php
+++ pocket-manager/app/Infrastructure/Http/Entity/Notification/Headers.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Http\Entity\Notification;
 
+use App\Domain\Entity\Currency\Money;
 use App\Domain\ValueObject\Email;
 use App\Domain\ValueObject\Name;
 use App\Util\Sanitize;
@@ -14,8 +15,9 @@ class Headers
     public readonly Name $sender;
     public readonly Email $to;
     public readonly Name $receiver;
-    public readonly ?string $reply_to;
-    public readonly ?string $date;
+    public readonly Money $money;
+    public readonly ?Email $reply_to;
+    public readonly Carbon $date;
 
     public function __construct(
         string $subject,
@@ -23,20 +25,31 @@ class Headers
         Name $sender,
         Email $to,
         Name $receiver,
+        Money $money,
         ?string $reply_to = null,
-        ?string $date = null,
+        ?Carbon $date = null,
     ) {
         $this->subject = Sanitize::string($subject);
-        $this->date = Carbon::createFromFormat('Y-m-d H:i:s', Sanitize::string($date));
         $this->from = $from;
         $this->sender = $sender;
         $this->to = $to;
         $this->receiver = $receiver;
-        $this->reply_to = Sanitize::string($reply_to) ?? $this->from;
+        $this->money = $money;
+        $this->reply_to = !is_null($reply_to) ? Sanitize::string($reply_to) : $this->from;
+        $this->date = $date ?? Carbon::now();
     }
 
     public function toArray(): array
     {
-        return (array) $this;
+        return [
+            'subject' => $this->subject,
+            'from' => $this->from->value,
+            'sender' => $this->sender->value,
+            'to' => $this->to->value,
+            'receiver' => $this->receiver->value,
+            'money' => $this->money->toInt(),
+            'reply_to' => $this->reply_to->value,
+            'date' => $this->date->format('Y-m-d H:i:s')
+        ];
     }
 }
