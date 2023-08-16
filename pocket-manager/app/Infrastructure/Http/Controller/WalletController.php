87e1452da18e565eb08c285dcbacde97e8a17dae
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-16 08:12:16 -0300

    feat: create required methods

--- pocket-manager/app/Infrastructure/Http/Controller/WalletController.php
+++ pocket-manager/app/Infrastructure/Http/Controller/WalletController.php
@@ -2,8 +2,17 @@
 
 namespace App\Infrastructure\Http\Controller;
 
+use App\Domain\Entity\People\Person;
+use App\Domain\Entity\Pocket\Wallet;
+use App\Domain\ValueObject\Uuid;
 use App\Http\Controllers\Controller;
+use App\Infrastructure\Http\Entity\StatusCode;
+use App\Infrastructure\Http\Entity\Wallet\CreateWalletRequest;
+use App\Infrastructure\Mapper\Api\Pocket\Wallet as PocketWallet;
+use App\Infrastructure\Mapper\Api\Pocket\Wallets;
 use App\Service\WalletService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class WalletController extends Controller
 {
@@ -12,4 +21,44 @@ class WalletController extends Controller
     ) {
     }
 
+    public function create(CreateWalletRequest $createWalletRequest): Response
+    {
+        $this->walletService->create(
+            new Person(id: new Uuid($createWalletRequest->get('person')))
+        );
+
+        return response()->noContent(StatusCode::CREATED->value);
+    }
+
+    public function all(string $person): JsonResponse
+    {
+        $allWallets = new Wallets(
+            $this->walletService->all(
+                new Person(id: new Uuid($person))
+            )
+        );
+
+        return response()->json($allWallets->request());
+    }
+
+    public function main(string $person)
+    {
+        $wallet = new PocketWallet($this->walletService->main(
+            new Person(id: new Uuid($person))
+        ));
+
+        return response()->json($wallet->request());
+    }
+
+    public function setMain($wallet): Response
+    {
+        $this->walletService->setMain(
+            new Wallet(
+                id: new Uuid($wallet),
+                main: true
+            )
+        );
+
+        return response()->noContent(StatusCode::OK->value);
+    }
 }
