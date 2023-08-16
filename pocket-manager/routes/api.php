cfb89ba6fd04adca876637ff9aec3b28aba43430
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-16 08:21:09 -0300

    refact: add new routes

--- pocket-manager/routes/api.php
+++ pocket-manager/routes/api.php
@@ -3,6 +3,7 @@
 use App\Infrastructure\Http\Controller\BalanceController;
 use App\Infrastructure\Http\Controller\TransactionController;
 use App\Infrastructure\Http\Controller\WalletController;
+use App\Infrastructure\Http\Entity\StatusCode;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/balance/{person}/{wallet}", [BalanceController::class, 'balance']);
@@ -11,7 +12,29 @@ Route::get("/transaction/{id}", [TransactionController::class, 'detail']);
 Route::get("/transaction/history/{person}/{wallet}", [TransactionController::class, 'history']);
 Route::post("/transaction", [TransactionController::class, 'transaction']);
 
-Route::get("/wallet/person", [WalletController::class, 'all']);
-Route::get("/wallet/person", [WalletController::class, 'main']);
 Route::post("/wallet", [WalletController::class, 'create']);
-Route::patch("/wallet/main", [WalletController::class, 'setMain']);
+Route::get("/wallet/all/person/{person}", [WalletController::class, 'all']);
+Route::get("/wallet/main/person/{person}", [WalletController::class, 'main']);
+Route::patch("/wallet/main/{wallet}", [WalletController::class, 'setMain']);
+
+
+
+################################################################################
+########## MOCKS
+Route::post("/can-transact", function () {
+    return response()->json(['pode dale!']);
+    // return response()->noContent(StatusCode::FORBIDDEN->value);
+});
+
+Route::get("/person/{person}", function () {
+    return response()->json([
+        'name' => 'Maicol Kaiser',
+        'email' => 'eu@duvidei.com.br'
+    ]);
+    // return response()->noContent(StatusCode::NOT_FOUND->value);
+});
+
+Route::get("/can-do-transfer/{person}/", function () {
+    return response()->noContent(StatusCode::ACCEPTED->value);
+    // return response()->noContent(StatusCode::FORBIDDEN);
+});
