abec4216d129423c2aba919679194e114a510d26
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-16 08:22:17 -0300

    feat: add feature tests

--- pocket-manager/tests/Feature/BalanceTest.php
+++ pocket-manager/tests/Feature/BalanceTest.php
@@ -6,6 +6,7 @@ use App\Domain\Entity\Currency\Money;
 use App\Domain\Repository\PersonRepositoryInterface;
 use App\Domain\Entity\Pocket\Wallet;
 use App\Domain\ValueObject\Uuid;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Tests\TestCase;
 
 class BalanceTest extends TestCase
@@ -30,23 +31,15 @@ class BalanceTest extends TestCase
         ]);
     }
 
-    public function test_cant_receive_balance(): void
+    public function test_not_found_balance(): void
     {
         $this->mock(PersonRepositoryInterface::class)
             ->shouldReceive('getWallet')
             ->once()
-            ->andReturn(
-                new Wallet(
-                    id: new Uuid('fbcd05e1-9530-44e2-b0a2-de551b260c18'),
-                    money: new Money(1500),
-                    main: true
-                )
-            );
+            ->andThrows(ModelNotFoundException::class);
 
         $response = $this->get('/api/balance/5cb0b7ec-07a2-4185-a8ca-ff9160ec4d2c/fbcd05e1-9530-44e2-b0a2-de551b260c18');
 
-        $response->assertJson([
-            'balance' => 1500
-        ]);
+        $response->assertNotFound();
     }
 }
--- pocket-manager/tests/Feature/TransactionTest.php
+++ pocket-manager/tests/Feature/TransactionTest.php
@@ -4,14 +4,20 @@ namespace Tests\Feature;
 
 use App\Domain\Entity\Financial\Transaction;
 use App\Domain\Entity\Currency\Money;
+use App\Domain\Entity\Financial\TransactionHistory;
+use App\Domain\Entity\Financial\Transactions;
+use App\Domain\Repository\PersonServiceInterface;
 use App\Domain\Repository\TransactionRepositoryInterface;
+use App\Domain\Repository\WalletRepositoryInterface;
 use App\Domain\ValueObject\Uuid;
+use App\Service\Interfaces\WalletServiceInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class TransactionTest extends TestCase
 {
-    public function test_can_receive_a_valid_transaction(): void
+    public function test_can_get_a_transaction_detail(): void
     {
         $this->mock(TransactionRepositoryInterface::class)
             ->shouldReceive('detail')
@@ -41,4 +47,130 @@ class TransactionTest extends TestCase
         ]);
     }
 
+    public function test_not_found_transaction(): void
+    {
+        $this->mock(TransactionRepositoryInterface::class)
+            ->shouldReceive('detail')
+            ->once()
+            ->andThrows(ModelNotFoundException::class);
+
+        $response = $this->get('/api/transaction/7685ceb6-292d-4fa4-a2d2-2e0a78f8dac7');
+
+        $response->assertNotFound();
+    }
+
+    public function test_can_get_a_transaction_history(): void
+    {
+        $transactions = [
+            [
+                'from' => 'fbcd05e1-9530-44e2-b0a2-de551b260c18',
+                'to' => '1b24b5e7-81e3-406a-b065-18ff9dcd5f38',
+                'value' => '199',
+                'created_at' => '2008-12-15 00:00:00',
+                'updated_at' => '2008-12-15 00:00:00',
+                'id' => '0e3e9c6d-5dfe-4c19-a33c-2a4ed8e39e02'
+            ],
+            [
+                'from' => 'fbcd05e1-9530-44e2-b0a2-de551b260c18',
+                'to' => '8c188ab4-b828-45bd-b84a-ddeb6462acff',
+                'value' => '599',
+                'created_at' => '2012-12-15 00:00:00',
+                'updated_at' => '2012-12-15 00:00:00',
+                'id' => 'cfa5d707-53aa-430c-aa7f-b819ae870dd4'
+            ]
+        ];
+
+        $this->mock(TransactionRepositoryInterface::class)
+            ->shouldReceive('history')
+            ->once()
+            ->andReturn(
+                TransactionHistory::toEntity(
+                    new Transactions($transactions)
+                )
+            );
+
+        $this->mock(WalletServiceInterface::class)
+            ->shouldReceive(['needExists', 'belongsToPerson'])
+            ->andReturnTrue();
+
+        $this->mock(PersonServiceInterface::class)
+            ->shouldReceive('needExists')
+            ->andReturnTrue();
+
+        $response = $this
+            ->get('/api/transaction/history/5cb0b7ec-07a2-4185-a8ca-ff9160ec4d2c/fbcd05e1-9530-44e2-b0a2-de551b260c18');
+
+        $expectedHistory = [
+            [
+                '0e3e9c6d-5dfe-4c19-a33c-2a4ed8e39e02' => [
+                    'from' => 'fbcd05e1-9530-44e2-b0a2-de551b260c18',
+                    'to' => '1b24b5e7-81e3-406a-b065-18ff9dcd5f38',
+                    'value' => '199',
+                    'created_at' => '2008-12-15 00:00:00',
+                    'updated_at' => '2008-12-15 00:00:00'
+                ]
+            ],
+            [
+                'cfa5d707-53aa-430c-aa7f-b819ae870dd4' => [
+                    'from' => 'fbcd05e1-9530-44e2-b0a2-de551b260c18',
+                    'to' => '8c188ab4-b828-45bd-b84a-ddeb6462acff',
+                    'value' => '599',
+                    'created_at' => '2012-12-15 00:00:00',
+                    'updated_at' => '2012-12-15 00:00:00'
+                ]
+            ]
+        ];
+
+        $response->assertJson($expectedHistory);
+    }
+
+    public function test_cant_do_transaction_without_funds()
+    {
+        $transaction = [
+            "from_wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+            "to_wallet" => "65dacab7-375f-4628-a30b-c6c34eeb3268",
+            "value" => 111
+        ];
+
+        $this->mock(TransactionRepositoryInterface::class)
+            ->shouldReceive('haveFunds')
+            ->andReturn(false);
+
+        $response = $this->postJson('/api/transaction', $transaction);
+
+        $response->assertJson([
+            "error" => "The provided wallet does not have sufficient funds.",
+            "wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+            "transaction" => [
+                "from_wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+                "to_wallet" => "65dacab7-375f-4628-a30b-c6c34eeb3268",
+                "value" => 111
+            ]
+        ])->assertForbidden();
+    }
+
+    public function test_cant_do_transfer_with_inexistent_wallet()
+    {
+        $transaction = [
+            "from_wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+            "to_wallet" => "65dacab7-375f-4628-a30b-c6c34eeb3268",
+            "value" => 111
+        ];
+
+        $this->mock(WalletRepositoryInterface::class)
+            ->shouldReceive('exists')
+            ->andReturn(false);
+
+        $response = $this->postJson('/api/transaction', $transaction);
+
+        $response->assertJson([
+            "error" => "The wallet you entered does not exist.",
+            "wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+            "transaction" => [
+                "from_wallet" => "fbcd05e1-9530-44e2-b0a2-de551b260c18",
+                "to_wallet" => "65dacab7-375f-4628-a30b-c6c34eeb3268",
+                "value" => 111
+            ]
+        ])->assertNotFound();
+    }
 }
