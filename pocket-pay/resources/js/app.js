7f04589f32c445274e385c9b8a80c962257306c1
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-19 13:30:14 -0300

    feat: add jquery and jmask

--- pocket-pay/resources/js/app.js
+++ pocket-pay/resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
-
+import $ from 'jquery';
 import Alpine from 'alpinejs';
+import mask from 'jquery-mask-plugin';
+
+window.$ = window.jQuery = $;
 
 window.Alpine = Alpine;
 
