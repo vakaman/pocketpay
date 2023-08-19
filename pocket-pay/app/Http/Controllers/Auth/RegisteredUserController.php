7bea6abe931d83eed06ebfea9d7c89542621deb9
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-19 13:33:16 -0300

    feat: get types from model

--- pocket-pay/app/Http/Controllers/Auth/RegisteredUserController.php
+++ pocket-pay/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -20,7 +21,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        return view('auth.register', ['types' => Type::all()]);
     }
 
     /**
