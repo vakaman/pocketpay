d6f04aa43a93908b26f1d43bb5a5cfb399e904df
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-20 19:08:30 -0300

    refact: adjust register to create required data

--- pocket-pay/app/Http/Controllers/Auth/RegisteredUserController.php
+++ pocket-pay/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Type;
+use App\Models\Person;
+use App\Models\PersonLegal;
+use App\Models\PersonNatural;
 use App\Models\User;
+use App\Models\UserPerson;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -13,40 +16,95 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Ramsey\Uuid\Uuid;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
     public function create(): View
     {
-        return view('auth.register', ['types' => Type::all()]);
+        return view('auth.register');
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        $this->validateRequest($request);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $user = $this->createUser($request);
+
+        $personUuid = Uuid::uuid4()->toString();
+
+        $this->createPerson($request, $personUuid);
+
+        $this->attachUserToPerson($user, $personUuid);
 
         event(new Registered($user));
 
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    private function validateRequest($request)
+    {
+        $request->validate([
+            'personType' => ['required', 'int'],
+            'economicActivities' => ['required', 'int'],
+            'name' => ['required', 'string', 'max:255'],
+            'document' => [
+                'required',
+                'string',
+                'max:18',
+                'unique:' . PersonLegal::class,
+                'unique:' . PersonNatural::class,
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:' . User::class,
+                'unique:' . PersonLegal::class,
+                'unique:' . PersonNatural::class,
+            ],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+    }
+
+    private function createUser($request)
+    {
+        return User::firstOrCreate(
+            ['email' => $request->email,],
+            [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]
+        );
+    }
+
+    private function createPerson($request, $personUuid): Person
+    {
+        return Person::firstOrCreate(
+            ['id' => $personUuid],
+            [
+                'id' => $personUuid,
+                'type_id' => $request->get('personType'),
+                'economic_activities_id' => $request->get('economicActivities'),
+            ],
+        );
+    }
+
+    private function attachUserToPerson($user, $personUuid)
+    {
+        UserPerson::firstOrCreate(
+            [
+                'user_id' => $user->id,
+                'person_id' => $personUuid,
+            ],
+            [
+                'user_id' => $user->id,
+                'person_id' => $personUuid,
+            ]
+        );
     }
 }
