dea7bef6f80a4f87f5f7fc94ac313c1b6305b6ec
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-20 22:07:30 -0300

    feat: create middleware to logged user

---
+++ pocket-pay/app/Http/Middleware/LoggedUserSession.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoggedUserSession
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (auth()->check()) {
+            $user = auth()->user();
+
+            Session::put([
+                'user' => [
+                    'teste' => '123'
+                ]
+            ]);
+
+            dd(auth());
+        }
+
+        return $next($request);
+    }
+}
