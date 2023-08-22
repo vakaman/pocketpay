2ee4b82d093c3d03ed9e18c5ba75edd748ffa1f6
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-21 23:24:28 -0300

    feat: create person entityes

--- pocket-pay/app/Domain/Entity/People/PersonLegal.php
+++ pocket-pay/app/Domain/Entity/People/PersonLegal.php
@@ -2,22 +2,45 @@
 
 namespace App\Domain\Entity\People;
 
+use App\Domain\Entity\EconomicActivities\EconomicActivities;
 use App\Domain\ValueObject\Cnpj;
 use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\Name;
+use App\Domain\ValueObject\Uuid;
 
-class PersonLegal
+class PersonLegal extends Person
 {
-    public readonly string $name;
+    public readonly Name $name;
     public readonly Cnpj $document;
     public readonly Email $email;
+    public readonly EconomicActivities $economicActivities;
 
     public function __construct(
-        string $name,
+        Uuid|string $id,
+        Type $type,
+        Name $name,
         Cnpj $document,
-        Email $email
+        Email $email,
     ) {
+        parent::__construct(id: $id, type: $type);
         $this->name = $name;
         $this->document = $document;
         $this->email = $email;
     }
+
+    public static function toEntity(array $model): Person
+    {
+        return new self(
+            id: new Uuid($model['id']),
+            type: new Type(id: $model['type_id']),
+            name: new Name(name: $model['legal']['name']),
+            document: new Cnpj(cnpj: $model['legal']['document']),
+            email: new Email(email: $model['legal']['email'])
+        );
+    }
+
+    // public static function fromEntity(Person $person): array
+    // {
+    //     return [];
+    // }
 }
--- pocket-pay/app/Domain/Entity/People/PersonNatural.php
+++ pocket-pay/app/Domain/Entity/People/PersonNatural.php
@@ -4,20 +4,41 @@ namespace App\Domain\Entity\People;
 
 use App\Domain\ValueObject\Cpf;
 use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\Name;
+use App\Domain\ValueObject\Uuid;
 
-class PersonNatural
+class PersonNatural extends Person
 {
-    public readonly string $name;
+    public readonly Name $name;
     public readonly Cpf $document;
     public readonly Email $email;
 
     public function __construct(
-        string $name,
+        Uuid|string $id,
+        Type $type,
+        Name $name,
         Cpf $document,
         Email $email
     ) {
+        parent::__construct(id: $id, type: $type);
         $this->name = $name;
         $this->document = $document;
         $this->email = $email;
+    }
+
+    public static function toEntity(array $model): Person
+    {
+        return new self(
+            id: new Uuid($model['id']),
+            type: new Type(id: $model['type_id']),
+            name: new Name(name: $model['natural']['name']),
+            document: new Cpf(cpf: $model['natural']['document']),
+            email: new Email(email: $model['natural']['email']),
+        );
+    }
+
+    public static function fromEntity(Person $person): array
+    {
+        return [];
     }
 }
