6e04bea0303ab33828198d155b33ea2db899a823
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-21 23:27:54 -0300

    refact: add informations in session

--- pocket-pay/app/Http/Middleware/LoggedUserSession.php
+++ pocket-pay/app/Http/Middleware/LoggedUserSession.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Domain\Repository\PeopleRepositoryInterface;
+use App\Domain\ValueObject\Uuid;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -9,16 +11,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LoggedUserSession
 {
+    public function __construct(
+        private PeopleRepositoryInterface $peopleRepository
+    ) {
+    }
+
     public function handle(Request $request, Closure $next): Response
     {
-
         if (auth()->check()) {
             $user = auth()->user();
 
             Session::put([
-                'user' => [
-                    'teste' => '123'
-                ]
+                'person' => $this->peopleRepository->get(
+                    new Uuid($user->person()->first()->id)
+                )
             ]);
         }
 
