c841833c4669d4e9830b202468952ba0d90f55e6
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-21 23:25:08 -0300

    feat: create person type entity

--- pocket-pay/app/Domain/Entity/People/Type.php
+++ pocket-pay/app/Domain/Entity/People/Type.php
@@ -2,11 +2,23 @@
 
 namespace App\Domain\Entity\People;
 
+use App\Domain\Enum\PersonTypeEnum;
+
 class Type
 {
+    public readonly int $id;
+    public readonly string|null $name;
+
     public function __construct(
-        public readonly int $id,
-        public readonly string $name,
+        int $id,
+        string|null $name = null
     ) {
+        $this->id = $id;
+        $this->name = $name ?? $this->getName($id);
+    }
+
+    private function getName(int $id): string
+    {
+        return PersonTypeEnum::from($id)->name;
     }
 }
