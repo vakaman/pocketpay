d96b0e85326e771719b9c77ee8ba2143aa6e1696
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-24 23:32:37 -0300

    feat: add lang files

---
+++ pocket-pay/lang/en/generic.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'Success' => 'Sucesso',
+    'Error' => 'Erro',
+    'Close' => 'Fechar',
+    'Ops... something is wrong!' => 'Ops... algo deu errado!',
+
+];
---
+++ pocket-pay/lang/en/wallet.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'Funds' => 'Fundos',
+    'Main' => 'Principal',
+    'wallet' => 'Carteira',
+    'Wallet created with success!' => 'Carteira criada com sucesso!',
+    'Wallet cannot be created to person.' => 'Não foi possível criar a carteira para a pessoa!',
+
+];
