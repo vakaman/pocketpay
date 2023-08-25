7d23162a9297f22cae2dbe3fd9abec9173e8af4a
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-24 23:28:26 -0300

    feat: add custom colors and enable jit

--- pocket-pay/tailwind.config.js
+++ pocket-pay/tailwind.config.js
@@ -3,7 +3,10 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    mode: 'jit',
     content: [
+        './public/**/*.html',
+        './public/**/*.{js,jsx,ts,tsx,vue}',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -14,6 +17,27 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                red: {
+                    100: '#fee2e2',
+                    200: '#fecaca',
+                    300: '#fca5a5',
+                    400: '#f87171',
+                },
+                gray: {
+                    50: '#f9fafb',
+                    100: '#f3f4f6',
+                    200: '#e5e7eb',
+                    300: '#d1d5db',
+                    400: '#9ca3af',
+                    500: '#6b7280',
+                    600: '#4b5563',
+                    700: '#374151',
+                    800: '#1f2937',
+                    900: '#111827',
+                    950: '#030712',
+                },
+            }
         },
     },
 
