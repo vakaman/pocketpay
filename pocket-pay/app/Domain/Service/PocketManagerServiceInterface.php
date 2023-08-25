075ec0b08cc9c063a4f8169c5c45a84620c5ec22
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-24 23:35:39 -0300

    feat: create pocket manager service interface

--- pocket-pay/app/Domain/Service/PocketManagerServiceInterface.php
+++ pocket-pay/app/Domain/Service/PocketManagerServiceInterface.php
@@ -2,9 +2,18 @@
 
 namespace App\Domain\Service;
 
+use App\Domain\Entity\People\PersonAbstract;
+use App\Domain\Entity\Pocket\Wallet;
+use App\Domain\Entity\Pocket\Wallets;
+
 /**
  * @see \App\Infrastructure\Service\PocketManagerService
  */
 interface PocketManagerServiceInterface
 {
+    public function list(PersonAbstract $person): Wallets;
+
+    public function walletCreate(PersonAbstract $person): Wallet;
+
+    public function walletDelete(Wallet $wallet): void;
 }
