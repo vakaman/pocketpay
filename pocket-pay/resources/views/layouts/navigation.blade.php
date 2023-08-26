33a681750bac00677f7e99ed7c69193177249c29
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-26 00:35:30 -0300

    refact: wallets

--- pocket-pay/resources/views/layouts/navigation.blade.php
+++ pocket-pay/resources/views/layouts/navigation.blade.php
@@ -20,7 +20,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('profile.wallet.history')" :active="request()->routeIs('dashboard')">
-                        {{ __('Wallet') }}
+                        {{ __('Wallets') }}
                     </x-nav-link>
                 </div>
             </div>
