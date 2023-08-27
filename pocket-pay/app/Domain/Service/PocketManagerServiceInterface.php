2c42ec743abee0ec7f839b15903b5b1a6a9dca66
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-08-27 00:44:13 -0300

    feat: add history method

--- pocket-pay/app/Domain/Service/PocketManagerServiceInterface.php
+++ pocket-pay/app/Domain/Service/PocketManagerServiceInterface.php
@@ -5,6 +5,7 @@ namespace App\Domain\Service;
 use App\Domain\Entity\People\PersonAbstract;
 use App\Domain\Entity\Pocket\Wallet;
 use App\Domain\Entity\Pocket\Wallets;
+use App\Domain\Entity\Financial\TransactionHistory as FinancialTransactionHistory;
 
 /**
  * @see \App\Infrastructure\Service\PocketManagerService
@@ -16,4 +17,6 @@ interface PocketManagerServiceInterface
     public function walletCreate(PersonAbstract $person): Wallet;
 
     public function walletDelete(Wallet $wallet): void;
+
+    public function history(PersonAbstract $person): FinancialTransactionHistory;
 }
