864a072f659b9a54a6c4eefb7e0eb0357dcc956d
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-09-11 16:56:54 -0300

    refact: adjust active link

--- pocket-pay/resources/views/layouts/navigation.blade.php
+++ pocket-pay/resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('profile.wallet.history')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('profile.wallet.history')" :active="request()->routeIs('profile.wallet.history')">
                         {{ __('Wallets') }}
                     </x-nav-link>
                 </div>
